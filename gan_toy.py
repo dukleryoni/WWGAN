722424cd780dd6fff5f7ce7237ec59ada7221988
dukleryoni <dukleryoni@gmail.com>
2018-12-28 18:22:19 -0800

    python3

--- gan_toy.py
+++ gan_toy.py
@@ -241,7 +241,7 @@ for iteration in range(ITERS):
         p.requires_grad = True  # they are set to False below in netG update
 
     for iter_d in range(CRITIC_ITERS):
-        _data = data.next()
+        _data = next(data)
         real_data = torch.Tensor(_data)
         if use_cuda:
             real_data = real_data.cuda()
@@ -281,7 +281,7 @@ for iteration in range(ITERS):
             p.requires_grad = False  # to avoid computation
         netG.zero_grad()
 
-        _data = data.next()
+        _data = next(data)
         real_data = torch.Tensor(_data)
         if use_cuda:
             real_data = real_data.cuda()
